3c9b42d79404a3c0bcc47f4f82a66ca3705f4419
gustavoveras <gustavoveras2013@gmail.com>
2016-05-25 21:23:06 -0300

    Implementando Fragment!

---
+++ app/src/main/java/com/projetogustavo/projeto_gustavo/Hero.java
@@ -0,0 +1,8 @@
+package com.projetogustavo.projeto_gustavo;
+
+/**
+ * Created by gustavoveras on 25/05/16.
+ */
+public class Hero {
+
+}
---
+++ app/src/main/java/com/projetogustavo/projeto_gustavo/HeroesFragment.java
@@ -0,0 +1,29 @@
+package com.projetogustavo.projeto_gustavo;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+
+public class HeroesFragment extends Fragment {
+
+    List<Hero> atbHeroes;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_heroes, container, false);
+    }
+
+}
